a57dedcd25819fde1b64f22d67f5efe1b917ed69
Stav Geffen <stavgeffen@gmail.com>
2016-06-25 11:38:34 +0300

    Better Heroku check

--- server/db/index.js
+++ server/db/index.js
@@ -1,4 +1,6 @@
 const level = require('./level');
 const mongo = require('./mongo');
 
-module.exports = process.env.HEROKU ? mongo() : level();
+const isHeroku = require('@panstav/is-heroku');
+
+module.exports = isHeroku() ? mongo() : level();
